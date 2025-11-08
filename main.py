382f6135f70cdb3ba04f39836e67929be48cabd5
Richard Herrera <yrichardz3003@gmail.com>
2025-11-08 18:19:50 -0300

    Primeiro commit do projeto

---
+++ main.py
@@ -0,0 +1,5 @@
+# Janela -> 500x500
+# Título -> "Conversor de Moedas"
+# Campos de selecionar moedas de origem e destino (Campo de lista), com labels "Selecione a moeda de origem!"
+# Botão de para converter as moedas, "Converter"
+# Lista de exibição com nome das moedas
